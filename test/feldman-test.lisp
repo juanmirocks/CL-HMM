10a505d0dc05a3b6ce38f837585b87795ece4615
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2013-11-18 14:23:59 +0100

    new protocol

--- test/feldman-test.lisp
+++ test/feldman-test.lisp
@@ -1,6 +1,42 @@
 (in-package :cl-hmm)
 
-(defun protocol-experiment (results-folder &key (states-power 5) (em-num-iterations 6) (max-times 300) (verbose-bws nil))
+(defun levenshtein (str1 str2)
+  "Calculates the Levenshtein distance between str1 and str2, returns an editing distance (int)."
+  (let ((n (length str1))
+        (m (length str2)))
+    ;; Check trivial cases
+    (cond ((= 0 n) (return-from levenshtein m))
+          ((= 0 m) (return-from levenshtein n)))
+    (let ((col (make-array (1+ m) :element-type 'integer))
+          (prev-col (make-array (1+ m) :element-type 'integer)))
+      ;; We need to store only two columns---the current one that
+      ;; is being built and the previous one
+      (dotimes (i (1+ m))
+        (setf (svref prev-col i) i))
+      ;; Loop across all chars of each string
+      (dotimes (i n)
+        (setf (svref col 0) (1+ i))
+        (dotimes (j m)
+          (setf (svref col (1+ j))
+                (min (1+ (svref col j))
+                     (1+ (svref prev-col (1+ j)))
+                     (+ (svref prev-col j)
+                        (if (equal (aref str1 i) (aref str2 j)) 0 1)))))
+        (rotatef col prev-col))
+      (svref prev-col m))))
+
+;; A few simple test-cases
+(assert (zerop (levenshtein "kitten" "kitten")))
+(assert (= (levenshtein "kitten" "") 6))
+(assert (= (levenshtein "kitten" "sitting") 3))
+(assert (= (levenshtein "" "") 0))
+(assert (= (levenshtein "" "a") 1))
+(assert (= (levenshtein "a" "") 1))
+
+(defun wer (reference recognized)
+  (min 1.0 (coerce (/ (levenshtein reference recognized) (length reference)) 'float)))
+
+(defun protocol-experiment (results-folder &key (training-size 10000) (num-translations 1000) (states-power 5) (em-num-iterations 6) (max-times 300) (verbose-bws nil))
        (multiple-value-bind (in L-size R-size) (read-pair-observations-file (pwd "test/resources/sample_feldman.txt"))
          (loop
             for power from 0 to states-power
@@ -8,18 +44,24 @@
             do
               (format t "~3%### Model with num states: ~d~3%" num-states)
               (let* ((phmm (make-random-phmm num-states L-size R-size))
-                     (training-data (subseq (mapcar #'(lambda (o) (cbook-encode phmm o)) in) 0 100))
-                     (test-data (mapcar #'(lambda (o) (cbook-encode phmm o)) (read-pair-observations-file (pwd "test/resources/test_feldman.txt")))))
+                     (training-data (subseq (mapcar #'(lambda (o) (cbook-encode phmm o)) in) 0 training-size))
+                     (test-data (mapcar #'(lambda (o) (cbook-encode phmm o)) (read-pair-observations-file (pwd "test/resources/test_feldman.txt"))))
+                     (test-size (length test-data)))
                 (multiple-value-bind (best loglikelihood)
                     (hmm-estimate-bws phmm training-data :threshold 0.05 :verbose-bws verbose-bws :starting-noise 0 :max-times max-times :iterations em-num-iterations :confidence 0)
-                  (hmm-save best (format nil "~a/model_~d_~f.lisp" results-folder num-states loglikelihood) :complete)
-                  (with-open-file (stream (format nil "~a/model_~d_~f.txt" results-folder num-states loglikelihood) :direction :output :if-exists :supersede)
+                  (hmm-save best (format nil "~a/model_~d_~d_~f.lisp" results-folder num-states training-size loglikelihood) :complete)
+                  (with-open-file (stream (format nil "~a/wer_~d_~d_~f.txt" results-folder num-states training-size loglikelihood) :direction :output :if-exists :supersede)
                     (loop for obs in test-data
-                       for x-encoded = (first obs)
-                       for x = (cbook-decode-left best x-encoded)
-                       for y = (cbook-decode-right best (hmm-translate best x-encoded))
+                       with wer = 0.0
                        do
-                         (loop for i below (1- (length x)) do (format stream "~d_" (elt x i))) (format stream "~d" (elt x (1- (length x))))
-                         (format stream " ")
-                         (loop for i below (1- (length y)) do (format stream "~d_" (elt y i))) (format stream "~d" (elt y (1- (length y))))
-                         (format stream "~%"))))))))
+                         (loop repeat num-translations
+                            with o_wer = 0.0
+                            for x = (first obs)
+                            for y = (second obs)
+                            for y-translation = (hmm-translate best x)
+                            do
+                              (incf o_wer (wer y y-translation))
+                            finally
+                              (incf wer (/ o_wer num-translations)))
+                       finally
+                         (format stream "~f~%" (/ wer test-size)))))))))
