990410646a6002a949329e1aded018a390e8a6e1
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2013-11-24 20:36:14 +0100

    the test is wrong

--- test/phmm-test.lisp
+++ test/phmm-test.lisp
@@ -1,23 +1,4 @@
 (in-package :cl-hmm)
 
 (defun test-forward&backward_are_symmetric ()
-  (semiring :log
-    (let* ((N 1)
-           (phmm (make-uniform-phmm N 1 1))
-           (obs (cbook-encode phmm (list (vector 0) (vector 0))))
-           (size_x (length (first obs)))
-           (size_y (length (second obs))))
-      (multiple-value-bind (o_p alpha) (forward-log phmm obs)
-        (multiple-value-bind (beta) (backward-log phmm obs)
-          (loop for l to size_x do
-               (loop for r to size_y do
-                    (when (< 0 (max l r))
-                      (loop for i below N
-                         with accum = ZERO do
-                           (setf accum (log+ accum (log* (alpha[] :log i l r) (beta[] :log i l r))))
-                         finally
-                           (unless (= accum o_p)
-                             (warn "Sum of alpha*beta (~d, ~d) must equal the probability, but ~a != ~a" l r accum o_p))))))
-          (values phmm (slot-value phmm 'B) o_p alpha beta))))))
-
-  (test-forward&backward_are_symmetric)
+  (warn "not implemented"))
